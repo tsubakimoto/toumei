7140f56cf1eee803bfebb29261b3201747d90fe8
tsubakimoto <tkmbmbr@gmail.com>
2015-02-12 21:24:21 +0900

    トラッキングコードを修正。

--- script.js
+++ script.js
@@ -73,16 +73,3 @@ function ConvertToHex(dec, alpha) {
   var hex = Math.round(i).toString(16);
   return ('0' + hex).slice(-2);
 }
-
-/*
- * Google Analyticsを読み込む
- */
-function LoadAnalytics() {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-30766436-3', 'auto');
-  ga('send', 'pageview');
-}
